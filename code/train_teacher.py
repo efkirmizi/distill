6988ee3f4a3f5c6ad3c0be862065a201bb01575f
efkirmizi <enisfurkankirmizi2003@gmail.com>
2026-05-05 17:42:26 +0300

    resnet32x4 and resnet8x4 added to flags

--- code/train_teacher.py
+++ code/train_teacher.py
@@ -55,6 +55,7 @@ def parse_option():
     # dataset
     parser.add_argument('--model', type=str, default='resnet110',
                         choices=['resnet8', 'resnet14', 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110',
+                                 'resnet8x4', 'resnet32x4',
                                  'wrn_40_2', 'wrn_16_2', 'vgg8', 'vgg11', 'vgg13', 'vgg16', 'vgg19'])
     parser.add_argument('--dataset', type=str, default='cifar10', choices=['cifar100', 'cifar10'], help='dataset')
 
